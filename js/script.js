fc7a192c74667c5900ff8a2502d34616bcc60c0b
Richard Gieg <richgieg@users.noreply.github.com>
2016-01-16 15:05:34 -0800

    Set background to Street View image

--- js/script.js
+++ js/script.js
@@ -12,8 +12,12 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
-    // YOUR CODE GOES HERE!
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+    $greeting.text('So, you want to live at ' + address + '?');
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     return false;
 };
